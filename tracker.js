0df0378a9754646e2286959c0b384904743ce767
Fred <none>
2025-03-12 20:14:31 +0000

    fixed some icons, added the base layout for the shop sanity and note tab

--- tracker.js
+++ tracker.js
@@ -47,3 +47,20 @@ boxes.forEach((box) => {
     newImg.classList.add("item");
   });
 });
+
+const shopFull = document.querySelector(".shop-full");
+const shopLate = document.querySelector(".shop-late");
+const swapBtns = document.querySelectorAll(".swap-btn");
+swapBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (shopLate.style.display === "grid") {
+      shopLate.style.display = "none";
+      shopFull.style.display = "grid";
+    } else {
+      shopLate.style.display = "none";
+      shopFull.style.display = "grid";
+    }
+    console.log(shopFull.style.display === "none");
+    console.log(shopLate.style.display === "none");
+  });
+});
